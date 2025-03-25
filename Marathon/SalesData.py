f1b89741a1082509b6c45ee34e74e98a0d7cac2b
RajCode12 <rjg64331@gmail.com>
2025-03-25 11:07:38 +0530

    Changes Updated in CSV file

--- Marathon/SalesData.py
+++ Marathon/SalesData.py
@@ -1,11 +1,11 @@
 import pandas as pd 
 
 # loading csv file data into dataframe
-df1 = pd.read_csv('sales_data_2022.csv')
+df1 = pd.read_csv('Marathon/sales_data_2022.csv')
 print(df1)
 print('*******************************************')
 
-df2 = pd.read_csv('sales_data_2023.csv')
+df2 = pd.read_csv('Marathon/sales_data_2023.csv')
 print(df2)
 print('*******************************************')
 
@@ -23,24 +23,23 @@ df3 = pd.DataFrame(data,columns=['Region','Product','Sales'])
 #print(df3)
 print('*******************************************')
 
-total_sales_by_region = merge_df.groupby('Region')['Sales'].sum()
 # total sales for each region 
-total_sales_by_region = df3.groupby('Region')['Sales'].sum()
+total_sales_by_region = merge_df.groupby('Region')['Sales'].sum()
 print('Total Sales by Region : ')
 print(total_sales_by_region)
 print('*******************************************')
 
 # total sales for each product
-total_sales_by_product = df3.groupby('Product')['Sales'].sum().reset_index()
+total_sales_by_product = merge_df.groupby('Product')['Sales'].sum().reset_index()
 print('Total Sales by Product : ')
 print(total_sales_by_product)
 print('*******************************************')
 
 # region with highest total sales
-highest_sales_region = df3.groupby('Region')['Sales'].sum().idxmax()
+highest_sales_region = merge_df.groupby('Region')['Sales'].sum().idxmax()
 print('Top Region : ', highest_sales_region)
 print('*******************************************')
 
 # product with highest total sales
-highest_sales_product = df3.groupby('Product')['Sales'].sum().idxmax()
+highest_sales_product = merge_df.groupby('Product')['Sales'].sum().idxmax()
 print('Top Product : ', highest_sales_product)
